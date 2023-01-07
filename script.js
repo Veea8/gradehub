502a9f5be4ebdcfb87a771d4026ba5819617fb0d
Veea8 <114304048+Veea8@users.noreply.github.com>
2023-01-07 10:42:19 +0100

    additional changes and grid layout

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
 //username
 var url = new URL(window.location.href);
 var username = url.searchParams.get("username");
-document.getElementById("welcome-name").innerHTML = username;
-document.getElementById("profile-name").innerHTML = username;
-document.getElementById("personal-information-name").innerHTML = username;
+document.getElementById("welcome-name").innerHTML = document.getElementById("profile-name").innerHTML = document.getElementById("personal-information-name").innerHTML = username;
 
 //countdown
-var holidaysDate = new Date("Feb 13, 2023 00:00:00").getTime();
-var holidaysCountdown = setInterval(function() {
-  var currentTime = new Date().getTime();
+var holidaysDate = new Date("Feb 13, 2023 00:00:00");
+var holidaysCountdown = setInterval(()=> {
+  var currentTime = new Date();
   var difference = holidaysDate - currentTime;
   var days = Math.floor(difference / (1000 * 60 * 60 * 24));
   var hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
