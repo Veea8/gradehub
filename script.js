4acd9872e450c7d6d124f8f1d1eaf44960de7d6e
Veea8 <114304048+Veea8@users.noreply.github.com>
2023-01-10 16:49:43 +0100

    responsive now

--- script.js
+++ script.js
@@ -17,3 +17,56 @@ var holidaysCountdown = setInterval(()=> {
     document.getElementById("holidays-countdown").innerHTML = "Still holidays";
   }
 }, 1000);
+
+//popup-menu
+const hamburgerMenu = document.querySelector(".hamburger-menu");
+const popupMenu = document.querySelector(".popup-menu");
+hamburgerMenu.addEventListener("click", () => {
+  if (popupMenu.style.display === "none") {
+    popupMenu.style.display = "block";
+  } else {
+    popupMenu.style.display = "none";
+  }
+});
+
+//popup profile
+const icon = document.querySelector(".profile-icon");
+const homeworkSection = document.querySelector(".homework-section-container");
+const testsSection = document.querySelector(".tests-section-container")
+icon.addEventListener("click", () => {
+  homeworkSection.classList.toggle("visible");
+  testsSection.classList.toggle("visible");
+  homeworkSection.style.position = testsSection.style.position = "absolute";
+  homeworkSection.style.top ="52px";
+  testsSection.style.top = "265px"
+  homeworkSection.style.width = testsSection.style.width = "100%"; 
+});
+
+/*tablet menu
+const hamburgerMenuTablet = document.querySelector(".hamburger-menu-tablet");
+const popupMenuTablet = document.querySelector(".popup-menu-tablet");
+hamburgerMenuTablet.addEventListener("click", () => {
+  if (popupMenuTablet.style.display === "none") {
+    popupMenuTablet.style.display = "block";
+  } else {
+    popupMenuTablet.style.display = "none";
+  }
+});*/
+
+// tablet menu
+const hamburgerMenuTablet = document.querySelector(".hamburger-menu-tablet");
+const popupMenuTablet = document.querySelector(".popup-menu-tablet");
+const sectionWidth = document.querySelector(".homework-section-container");
+const sectionWidthWidth = sectionWidth.offsetWidth;
+hamburgerMenuTablet.addEventListener("click", () => {
+  if (popupMenuTablet.style.display === "none") {
+    popupMenuTablet.style.display = "block";
+    popupMenuTablet.style.width = sectionWidthWidth + "px";
+    popupMenuTablet.style.position = "absolute";
+    popupMenuTablet.style.top = "73px";
+    popupMenuTablet.style.right = "-" + sectionWidthWidth + "px";
+  } else {
+    popupMenuTablet.style.display = "none";
+    popupMenuTablet.style.width = "";
+  }
+});
