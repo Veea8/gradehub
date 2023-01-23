272b7460e2663c8dfece0aa2cdd0d734666746f4
Veea8 <114304048+Veea8@users.noreply.github.com>
2023-01-23 20:26:38 +0100

    temporary final version

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-//username
+//get username and display on website
 var url = new URL(window.location.href);
 var username = url.searchParams.get("username");
 document.getElementById("welcome-name").innerHTML = document.getElementById("profile-name").innerHTML = document.getElementById("personal-information-name").innerHTML = username;
 
-//countdown
+//holidays countdown
 var holidaysDate = new Date("Feb 13, 2023 00:00:00");
 var holidaysCountdown = setInterval(()=> {
   var currentTime = new Date();
@@ -18,7 +18,7 @@ var holidaysCountdown = setInterval(()=> {
   }
 }, 1000);
 
-//popup-menu
+//popup-menu on mobile version
 const hamburgerMenu = document.querySelector(".hamburger-menu");
 const popupMenu = document.querySelector(".popup-menu");
 hamburgerMenu.addEventListener("click", () => {
@@ -29,33 +29,15 @@ hamburgerMenu.addEventListener("click", () => {
   }
 });
 
-//popup profile
+//popup sections on mobile version
 const icon = document.querySelector(".profile-icon");
-const homeworkSection = document.querySelector(".homework-section-container");
-const testsSection = document.querySelector(".tests-section-container")
+const homeworkSection = document.querySelector(".homework-section-container-popup");
+const testsSection = document.querySelector(".tests-section-container-popup");
 icon.addEventListener("click", () => {
   homeworkSection.classList.toggle("visible");
   testsSection.classList.toggle("visible");
   homeworkSection.style.position = testsSection.style.position = "absolute";
   homeworkSection.style.top ="52px";
-  testsSection.style.top = "265px"
+  testsSection.style.top = "265px";
   homeworkSection.style.width = testsSection.style.width = "100%"; 
-});
-
-// tablet menu
-const hamburgerMenuTablet = document.querySelector(".hamburger-menu-tablet");
-const popupMenuTablet = document.querySelector(".popup-menu-tablet");
-const sectionWidth = document.querySelector(".homework-section-container");
-const sectionWidthWidth = sectionWidth.offsetWidth;
-hamburgerMenuTablet.addEventListener("click", () => {
-  if (popupMenuTablet.style.display === "none") {
-    popupMenuTablet.style.display = "block";
-    popupMenuTablet.style.width = sectionWidthWidth + "px";
-    popupMenuTablet.style.position = "absolute";
-    popupMenuTablet.style.top = "73px";
-    popupMenuTablet.style.right = "-" + sectionWidthWidth + "px";
-  } else {
-    popupMenuTablet.style.display = "none";
-    popupMenuTablet.style.width = "";
-  }
 });
