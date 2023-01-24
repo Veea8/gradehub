17e64bb690cfd1545be0690227ef12a461741862
Veea8 <114304048+Veea8@users.noreply.github.com>
2023-01-24 15:23:18 +0100

    finished version so far

--- script.js
+++ script.js
@@ -1,23 +1,3 @@
-//get username and display on website
-var url = new URL(window.location.href);
-var username = url.searchParams.get("username");
-document.getElementById("welcome-name").innerHTML = document.getElementById("profile-name").innerHTML = document.getElementById("personal-information-name").innerHTML = username;
-
-//holidays countdown
-var holidaysDate = new Date("Feb 13, 2023 00:00:00");
-var holidaysCountdown = setInterval(()=> {
-  var currentTime = new Date();
-  var difference = holidaysDate - currentTime;
-  var days = Math.floor(difference / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
-  document.getElementById("holidays-countdown").innerHTML = days + "d " + hours + "h " + minutes + "m ";
-  if (difference < 0) {
-    clearInterval(holidaysCountdown);
-    document.getElementById("holidays-countdown").innerHTML = "Still holidays";
-  }
-}, 1000);
-
 //popup-menu on mobile version
 const hamburgerMenu = document.querySelector(".hamburger-menu");
 const popupMenu = document.querySelector(".popup-menu");
@@ -41,3 +21,23 @@ icon.addEventListener("click", () => {
   testsSection.style.top = "265px";
   homeworkSection.style.width = testsSection.style.width = "100%"; 
 });
+
+//holidays countdown
+var holidaysDate = new Date("Feb 13, 2023 00:00:00");
+var holidaysCountdown = setInterval(()=> {
+  var currentTime = new Date();
+  var difference = holidaysDate - currentTime;
+  var days = Math.floor(difference / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+  document.getElementById("holidays-countdown").innerHTML = days + "d " + hours + "h " + minutes + "m ";
+  if (difference < 0) {
+    clearInterval(holidaysCountdown);
+    document.getElementById("holidays-countdown").innerHTML = "Still holidays";
+  }
+}, 1000);
+
+//get username and update title
+var url = new URL(window.location.href);
+var username = url.searchParams.get("username");
+document.getElementById("welcome-name").innerHTML = document.getElementById("profile-name").innerHTML = document.getElementById("personal-information-name").innerHTML = username;
